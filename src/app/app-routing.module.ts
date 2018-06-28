37438fa83f14a680684b8eeee6afbc63fc375ad1
KouiDeveloper <kouiprogramer@gmail.com>
2018-06-28 21:19:01 +0700

    update popup winner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { A000MainLayoutComponent } from './a000-main-layout/a000-main-layout.component';
 import { A002IndexComponent } from './a002-index/a002-index.component';
 import { A003RandomComponent } from './a003-random/a003-random.component';
+import { A004PopupTheWinnerComponent } from './a004-popup-the-winner/a004-popup-the-winner.component';
 
 const routes: Routes = [
   { path: 'main-laout', component: A000MainLayoutComponent},
   { path: 'index', component: A002IndexComponent},
-  { path: 'ran-dom', component: A003RandomComponent}
+  { path: 'ran-dom', component: A003RandomComponent},
+  { path: 'winner', component: A004PopupTheWinnerComponent}
 ]
 @NgModule({
   imports: [
