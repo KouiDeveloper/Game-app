a3ed04fd1fde269dbe5bafaaf5c4219498ba6271
KouiDeveloper <kouiprogramer@gmail.com>
2018-06-28 14:58:09 +0700

    update index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { A000MainLayoutComponent } from './a000-main-layout/a000-main-layout.component';
+import { A002IndexComponent } from './a002-index/a002-index.component';
 
 const routes: Routes = [
-  { path: 'main-laout', component: A000MainLayoutComponent}
+  { path: 'main-laout', component: A000MainLayoutComponent},
+  { path: 'index', component: A002IndexComponent}
 ]
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports:[
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { A001HeaderComponent } from './a001-header/a001-header.component';
 import { NgxPopper } from 'angular-popper';
 import { A000MainLayoutComponent } from './a000-main-layout/a000-main-layout.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { A002IndexComponent } from './a002-index/a002-index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     A001HeaderComponent,
-    A000MainLayoutComponent
+    A000MainLayoutComponent,
+    A002IndexComponent
   ],
   imports: [
     BrowserModule,
