df9fbd2d62d8556e371854c12b92e4ed8fd2fc20
KouiDeveloper <kouiprogramer@gmail.com>
2018-06-29 14:18:30 +0700

    update css in bootstrap and create admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { A002IndexComponent } from './a002-index/a002-index.component';
 import { A003RandomComponent } from './a003-random/a003-random.component';
 import { A004PopupTheWinnerComponent } from './a004-popup-the-winner/a004-popup-the-winner.component';
 import { B005ProlistComponent } from './b005-prolist/b005-prolist.component';
+import { B006AdminComponent } from './b006-admin/b006-admin.component';
 
 const routes: Routes = [
   { path: 'main-laout', component: A000MainLayoutComponent},
   { path: 'index', component: A002IndexComponent},
   { path: 'ran-dom', component: A003RandomComponent},
   { path: 'winner', component: A004PopupTheWinnerComponent},
-  { path: 'promotion', component:B005ProlistComponent}
+  { path: 'promotion', component:B005ProlistComponent},
+  { path:'admin-control', component: B006AdminComponent}
 ]
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { A002IndexComponent } from './a002-index/a002-index.component';
 import { A003RandomComponent } from './a003-random/a003-random.component';
 import { A004PopupTheWinnerComponent } from './a004-popup-the-winner/a004-popup-the-winner.component';
 import { B005ProlistComponent } from './b005-prolist/b005-prolist.component';
+import { B006AdminComponent } from './b006-admin/b006-admin.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { B005ProlistComponent } from './b005-prolist/b005-prolist.component';
     A002IndexComponent,
     A003RandomComponent,
     A004PopupTheWinnerComponent,
-    B005ProlistComponent
+    B005ProlistComponent,
+    B006AdminComponent
   ],
   imports: [
     BrowserModule,
